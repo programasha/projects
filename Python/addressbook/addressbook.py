8b128a6a580f1ed75b6bfa7fee03ca45182ce1dd
Masha Ushakova <masha.a.ushakova@icloud.com>
2019-03-24 00:17:11 +0300

    addressbook version 0.2

--- Python/addressbook/addressbook.py
+++ Python/addressbook/addressbook.py
@@ -1,39 +1,53 @@
 d = {}
 while True:
-    action = input("What do you want to do?(add, search, delete, edit)")
-    name = input("Write the name")
+    action = input("Enter command (add, search, delete, edit, exit, print): ")
     if action == "add":
-        num = input("Write the number")
+        name = input("Enter the name: ")
+        num = input("Enter the number: ")
         if name in d:
-            ans = input("Do you want to change?")
+            ans = input("Do you want to change? ")
             if ans == "no":
                 name = name + "*"
         d[name] = num
-        print(d)
     elif action == "search":
+        name = input("Enter the name: ")
         if name in d:
-            print(d[name])
+            print(name + " has number " + d[name])
         else:
-            print("NO")
+            print("Name \"" + name + "\" not found")
     elif action == "delete":
+        name = input("Enter the name: ")
         if name in d:
             del d[name]
-        print(d)
+            print("\"" + name + "\" was deleted ")
+        else:
+            print("Name \"" + name + "\" not found")
     elif action == "edit":
-        ans = input("What do you want to change?")
+        name = input("Enter the name: ")
         if name in d:
-            if ans == "name":
-                num = d[name]
-                del d[name]
-                name = input("Write new name")
-                d[name] = num
-            elif ans == "number":
-                num = input("Write new number")
-                d[name] = num
-            print(d)
-    else:
-        print("Incorrect input. Try again.")
-    ans = input("Do you want to exit? (YES/NO)")
-    if ans == "YES":
+            ans = input("What do you want to change?(name, number) ")
+            if name in d:
+                if ans == "name":
+                    oldName = name
+                    num = d[name]
+                    del d[name]
+                    name = input("Enter new name: ")
+                    d[name] = num
+                    print("Name \"" + oldName + "\" was changed to \"" + name + "\"")
+                elif ans == "number":
+                    num = input("Enter new number: ")
+                    d[name] = num
+                    print("Name \"" + name + "\" has new number " + num)
+                else:
+                    print("Incorrect input ")
+        else:
+            print("Name \"" + name + "\" not found")
+    elif action == "exit":
+        print("Goodbye!")
         break
+    elif action == "print":
+        for key in d:
+            print(key + " : " + d[key])
+    else:
+        print("Incorrect input")
 
